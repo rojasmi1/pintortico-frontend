17763d48ddb6905619cf81ce0bd3c385250705a0
michaelrojasp@gmail.com <michaelrojasp@gmail.com>
2022-07-03 20:04:07 -0600

    Remove contentful.js and use fetch api instead

--- src/api/ArtWorks/index.ts
+++ src/api/ArtWorks/index.ts
@@ -1,27 +1,38 @@
-import { Category, ArtWork } from "../../types/contentful";
-import contentful from "contentful";
+import { ArtWork } from "../../types/contentful";
 
 const { CONTENTFUL_API_TOKEN, CONTENTFUL_SPACE_ID } = import.meta.env;
 
-const client = contentful.createClient({
-  space: CONTENTFUL_SPACE_ID,
-  accessToken: CONTENTFUL_API_TOKEN,
-});
-
 export const getArtWorksByCategory = async (categoryId: string) => {
-  const { items } = await client.getEntries<Category>(categoryId);
-
-  return items[0].fields.images;
+  const response = await (
+    await fetch(
+      `https://cdn.contentful.com/spaces/${CONTENTFUL_SPACE_ID}/entries/${categoryId}?access_token=${CONTENTFUL_API_TOKEN}`
+    )
+  ).json();
+  const artWorkLinks = response.fields.images;
+  const artWorks = await Promise.all(
+    artWorkLinks.map((artWorkLink) => getArtWork(artWorkLink.sys.id))
+  );
+  return artWorks;
 };
 export const getAllArtWorks = async () => {
-  const { items } = await client.getEntries<ArtWork>({
-    content_type: "image",
-    select: "sys.id",
-  });
-
-  return items;
+  const { items } = await (
+    await fetch(
+      `https://cdn.contentful.com/spaces/${CONTENTFUL_SPACE_ID}/entries?access_token=${CONTENTFUL_API_TOKEN}&content_type=image&select=sys.id`
+    )
+  ).json();
+  const artWorks = Promise.all(items.map((item) => getArtWork(item.sys.id)));
+  return artWorks;
 };
 export const getArtWork: (id: string) => Promise<ArtWork> = async (id) => {
-  const artWork = await client.getEntry<ArtWork>(id);
-  return artWork;
+  const artWork = await (
+    await fetch(
+      `https://cdn.contentful.com/spaces/${CONTENTFUL_SPACE_ID}/entries/${id}?access_token=${CONTENTFUL_API_TOKEN}`
+    )
+  ).json();
+  const photoDetails = await (
+    await fetch(
+      `https://cdn.contentful.com/spaces/${CONTENTFUL_SPACE_ID}/assets/${artWork.fields.photo.sys.id}?access_token=${CONTENTFUL_API_TOKEN}`
+    )
+  ).json();
+  return { ...artWork, fields: { ...artWork.fields, photo: photoDetails } };
 };
--- src/api/Categories/index.ts
+++ src/api/Categories/index.ts
@@ -1,18 +1,11 @@
-import { Category } from "../../types/contentful";
-import * as contentful from "contentful";
-
 const { CONTENTFUL_API_TOKEN, CONTENTFUL_SPACE_ID } = import.meta.env;
 
-const client = contentful.createClient({
-  space: CONTENTFUL_SPACE_ID,
-  accessToken: CONTENTFUL_API_TOKEN,
-});
-
 export const getCategories = async () => {
-  const { items: categories } = await client.getEntries<Category>({
-    content_type: "photoGallery",
-    select: "sys.id,fields.title",
-  });
+  const { items: categories } = await (
+    await fetch(
+      `https://cdn.contentful.com/spaces/${CONTENTFUL_SPACE_ID}/entries?access_token=${CONTENTFUL_API_TOKEN}&content_type=photoGallery&select=sys.id,fields.title`
+    )
+  ).json();
 
   return categories;
 };
